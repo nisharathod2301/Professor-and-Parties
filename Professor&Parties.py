9282f1d80e2e22af115c4369f7b5c8fe3a3cf50c
nisharathod2301 <nisharathod232001.com>
2022-07-22 18:51:48 +0530

    Using Hashset

--- Professor&Parties.py
+++ Professor&Parties.py
@@ -1,1 +1,11 @@
+def PartyType(self, a, n):
+    hashset =set()
+    for n in a:
+        if n in hashset:
+            return "BOYS"
+        hashset.add(n)
+    return "GIRLS"
 
+arr=input("Enter input:")
+num=arr.len
+PartyType(arr,num)
